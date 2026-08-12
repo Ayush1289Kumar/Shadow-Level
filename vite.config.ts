5112599d8b52f79444b8b151a4046a654a4018a9
Ayush1289Kumar <ayushkumarpro1289@gmail.com>
2026-08-12 20:21:23 +0530

    Added graphify

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Shadow-Level/',
   plugins: [
     TanStackRouterVite(),
     viteReact(),
